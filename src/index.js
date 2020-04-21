afaa11010da130bc33725fe70c6e8f73e822aacb
Om Shanker <omshanker@Oms-MacBook-Air-1384.local>
2020-04-21 14:18:33 +0530

    Updated Port

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-const port = process.env.PORT || 3000
+const port = 3000
 
 const staticpath = path.join(__dirname, '../public')
 
